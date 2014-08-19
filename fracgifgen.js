f5161e07817f66208f2af33f1895c679a1553dd5
Dan Zimmerman <daniel.zimmerman@me.com>
2014-08-19 04:38:15 -0500

    updated gif creator

--- fracgifgen.js
+++ fracgifgen.js
@@ -1,20 +1,67 @@
 #!/usr/bin/env node
 
-var util          = require('util'),
-    cp            = require('child_process'),
-    fs            = require('fs'),
-    i;
+var util      = require('util'),
+    cp        = require('child_process'),
+    fs        = require('fs'),
+    optparse  = require('optparse'),
+    i,
+    switches = [
+      ["-o", "--out DIR", "The directory to place all the images created"],
+      ["-p", "--prefix PREFIX", "The name for the files to be created"],
+      ["-c", "--color HEX", "The hex color for the images to be created"],
+      ["-d", "--delay NUMBER", "The delay for between each image in the resulting gif"],
+      ["-i", "--initial NUMBER", "The initial parameter to create the gif"],
+      ["-f", "--final NUMBER", "The final parameter to create the gif"],
+      ["-h", "--help", "Help"],
+      ["-g", "--generator something.fg", "The generator used to render the fractals"],
+      ["-m", "--multiplier NUMBER", "The number to multiply each value in [initial final] by to be the parameter"]
+    ],
+    parser = new optparse.OptionParser(switches);
 
 var outdir = "fracgif",
     outfilebase = "frac",
     color = "B562E0",
     delay = "10",
     initial = 20,
-    end = 200;
+    end = 200,
+    generator = null,
+    multiplier = 0.1;
+
+parser.on("out", function(d, dir) {
+  outdir = dir;
+});
+parser.on("prefix", function(d, pre) {
+  outfilebase = pre;
+});
+parser.on("color", function(d, col) {
+  color = col;
+});
+parser.on("delay", function(d, del) {
+  delay = del;
+});
+parser.on("initial", function(d, num) {
+  initial = num;
+});
+parser.on("final", function(d, fin) {
+  end = fin;
+});
+parser.on("generator", function(d, gen) {
+  generator = gen;
+});
+parser.on("multiplier", function(d, multi) {
+  multiplier = multi;
+});
+parser.on("help", function() {
+  console.log("This utility iterates through parameters from `initial` to `final` (inclusive on both) to create a series of pngs from `fractalgen`. It then creates a gif out of these png.");
+  console.log(parser.toString());
+  process.exit(0);
+});
+
+parser.parse(process.argv);
 
 var params = [];
 for (i = initial; i <= end; i++) {
-  params.push([i / 10, i]);
+  params.push([i * multiplier, i]);
 }
 
 try {
@@ -32,14 +79,14 @@ function createGif() {
         console.log("Error creating gif: " + error);
       }
     });
-  }, 1000);
+  }, 2500);
 }
 
 i = 0;
 
 params.forEach(function(param, i) {
   if (!fs.existsSync(outdir + "/" + outfilebase + param[1] + ".png")) {
-    var command = "./fractalgen -c " + color + " -p " + param[0] + " -o " + outdir + "/" + outfilebase + param[1] + ".png";
+    var command = "./fractalgen -c " + color + " -p " + param[0] + " -o " + outdir + "/" + outfilebase + param[1] + ".png" + (generator ? " -g " + generator : "");
     console.log(command);
     cp.exec(command, function(error, stdout, stderr) {
       i++;
