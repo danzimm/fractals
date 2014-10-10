1cd02c2151b0e891cd53da96ac084dd4703fd0a4
Dan Zimmerman <daniel.zimmerman@me.com>
2014-10-10 00:56:37 -0500

    fix up sier tri

--- kernels/sierpinski.cu
+++ kernels/sierpinski.cu
@@ -4,35 +4,27 @@
 extern "C" {
 
 __device__
+unsigned npnts = 3;
+
+__device__
+double2 pnts[] = {
+  {0.0, 0.0},
+  {1.0, 0.0},
+  {0.5, 0.86602540378}
+  //{1.0, 0.0},
+  //{0.0, 1.0}
+};
+
+__device__
 double2 fetch_initial_point(unsigned long i) {
-  i = i % 5;
-  switch(i) {
-    case 0:
-      return (double2){0.0,0.0};
-    case 1:
-      return (double2){1.0, 0.0};
-    case 2:
-      return (double2){0.0, 1.0};
-    case 3:
-      return (double2){1.0, 1.0};
-    case 4:
-      return (double2){0.5, 0.5};
-  }
-  return (double2){0.0, 0.0};
+  i = i % npnts;
+  return pnts[i];
 }
 
 __device__
 double2 iterate_point(double2 val, unsigned long i, double2 ipnt, unsigned long func_n) {
-  func_n = func_n % 3;
-  switch (func_n) {
-    case 0:
-      return 0.5 * val;
-    case 1:
-      return 0.5 * val + (double2){0.5, 0.0};
-    case 2:
-      return 0.5 * val + (double2){0.0, 0.5};
-  }
-  return val;
+  func_n = func_n % npnts;
+  return 0.5 * val + 0.5 * pnts[func_n];
 }
 
 }
